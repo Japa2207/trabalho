ae6ce0d06ed3998088561c4beb92226e2be06629
Japa2207 <eduardo.otramario.passos@escola.pr.gov.br>
2026-05-13 12:24:49 -0300

    .

--- main.js
+++ main.js
@@ -13,3 +13,33 @@ for(let i = 0; i<botoes.length; i++) {
         textos[i].classList.remove("ativo")
     }
 }
+
+
+const contadores = document.querySelectorAll.contador
+const tempoObjetivo1 = new Date("2026-10-05T00:00:00")
+const tempoObjetivo2 = new Date("2026-11-05T00:00:00")
+const tempoObjetivo3 = new Date("2026-12-05T00:00:00")
+const tempoObjetivo4 = new Date("2027-01-05T00:00:00")
+
+const tempos = tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+
+function calculaTempo( tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual
+    console.log(tempoFinal)
+    let segundos = Math.floor(tempoFinal / 1000)
+    let minutos = Math.floor(tempoFinal / 60)
+    let horas = Math.floor(tempoFinal / 60)
+    let dias = Math.floors(tempoFinal / 24)
+
+
+  segundos %= 60
+  minutos %= 60
+  horas %= 24
+  if (tempoFinal > 0 ){
+    return [dias, horas, minutos, segundos]
+
+    }else{
+        return[0,0,0,0]
+    }
+}
