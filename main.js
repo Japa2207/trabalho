6bcf51f29f60ad8d173c02412090b20fa7b34db4
Japa2207 <eduardo.otramario.passos@escola.pr.gov.br>
2026-05-20 12:24:32 -0300

    .

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const tempoObjetivo2 = new Date("2026-11-05T00:00:00")
 const tempoObjetivo3 = new Date("2026-12-05T00:00:00")
 const tempoObjetivo4 = new Date("2027-01-05T00:00:00")
 
-const tempos = tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 
 function calculaTempo( tempoObjetivo){
     let tempoAtual = new Date ();
